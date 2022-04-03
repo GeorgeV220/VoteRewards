9f944e268d2306187db1e3cec27e5da7c96f2d7a
George V <georgev22.github@gmail.com>
2022-04-03 14:21:37 +0300

    feat(NPCCommand): Fully working NPC command

--- src/main/java/com/georgev22/voterewards/commands/NPCCommand.java
+++ src/main/java/com/georgev22/voterewards/commands/NPCCommand.java
@@ -1,60 +1,123 @@
 package com.georgev22.voterewards.commands;
 
+import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.*;
+import com.georgev22.api.maps.ObjectMap;
 import com.georgev22.api.minecraft.MinecraftUtils;
 import com.georgev22.voterewards.VoteRewardPlugin;
+import com.georgev22.voterewards.hooks.NPCAPI;
 import com.georgev22.voterewards.utilities.MessagesUtil;
-import com.georgev22.voterewards.utilities.player.VoteUtils;
-import com.github.juliarn.npc.NPC;
-import com.github.juliarn.npc.NPCPool;
-import com.github.juliarn.npc.profile.Profile;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Random;
-import java.util.UUID;
 
 @CommandAlias("vnpc")
 public class NPCCommand extends Command {
 
     private final VoteRewardPlugin voteRewardPlugin = VoteRewardPlugin.getInstance();
 
-    private final NPCPool npcPool = NPCPool.builder(voteRewardPlugin)
-            .spawnDistance(60)
-            .actionDistance(30)
-            .tabListRemoveTicks(20)
-            .build();
+    @HelpCommand
+    @Subcommand("help")
+    @CommandAlias("vnpchelp")
+    @Description("{@@commands.descriptions.npc.help}")
+    @Override
+    public void onHelp(final CommandSender sender, @NotNull CommandHelp commandHelp, String @NotNull [] args) {
+        MinecraftUtils.msg(sender, "&c&l(!)&c Commands&c &l(!)");
+        MinecraftUtils.msg(sender, "&6/vnpc create");
+        MinecraftUtils.msg(sender, "&6/vnpc remove");
+        MinecraftUtils.msg(sender, "&6/vnpc update");
+        MinecraftUtils.msg(sender, "&6/vnpc updateall");
+        MinecraftUtils.msg(sender, "&c&l==============");
+    }
 
     @Default
     @Description("{@@commands.descriptions.npc}")
     @CommandPermission("voterewards.npc")
     public void execute(@NotNull final CommandSender sender, final String[] args) {
-        if (args.length == 0) {
+        MinecraftUtils.msg(sender, "&c&l(!)&c Commands&c &l(!)");
+        MinecraftUtils.msg(sender, "&6/vnpc create");
+        MinecraftUtils.msg(sender, "&6/vnpc remove");
+        MinecraftUtils.msg(sender, "&6/vnpc update");
+        MinecraftUtils.msg(sender, "&6/vnpc updateall");
+        MinecraftUtils.msg(sender, "&c&l==============");
+    }
+
+    @Subcommand("create")
+    @CommandAlias("vnpccreate")
+    @CommandCompletion("<npcname> @range:1-999")
+    @Description("{@@commands.descriptions.npc.create}")
+    @Syntax("create <npcname> <position>")
+    public void create(CommandSender sender, String @NotNull [] args) {
+        if (args.length <= 1) {
+            MinecraftUtils.msg(sender, "&c&l(!)&c Not enough arguments");
             return;
         }
 
-        if (args[0].equalsIgnoreCase("create")) {
-            if (args.length == 1) {
-                return;
-            }
-            if (!(sender instanceof Player player)) {
-                MessagesUtil.ONLY_PLAYER_COMMAND.msg(sender);
-                return;
-            }
-            Profile profile = new Profile(VoteUtils.getTopPlayer(Integer.parseInt(args[1])));
-            profile.complete();
-
-            profile.setUniqueId(new UUID(new Random().nextLong(), 0));
-
-            NPC.builder()
-                    .profile(profile)
-                    .location(player.getLocation())
-                    .imitatePlayer(false)
-                    .lookAtPlayer(true)
-                    .build(npcPool);
-
-            MinecraftUtils.msg(sender, "&a&l(!)&a Successfully created npc!");
+        if (!(sender instanceof Player player)) {
+            MessagesUtil.ONLY_PLAYER_COMMAND.msg(sender);
+            return;
         }
+
+        if (NPCAPI.npcExists(args[0])) {
+            MinecraftUtils.msg(sender, "&c&l(!)&c NPC " + args[1] + " already exists!");
+            return;
+        }
+
+        if (!args[1].matches("-?(0|[1-9]\\d*)")) {
+            MinecraftUtils.msg(sender, "&c&l(!)&c The position must be a number (1 - +∞)");
+            return;
+        }
+
+        NPCAPI.create(args[0], Integer.parseInt(args[1]), player.getLocation(), true);
+
+        MinecraftUtils.msg(sender, "&a&l(!)&a Successfully created " + args[0] + " npc with position " + args[1] + "!");
+    }
+
+    @Subcommand("remove")
+    @CommandAlias("vnpcremove")
+    @CommandCompletion("<npcname>")
+    @Description("{@@commands.descriptions.npc.remove}")
+    @Syntax("remove <npcname>")
+    public void remove(CommandSender sender, String @NotNull [] args) {
+        if (args.length == 0) {
+            MinecraftUtils.msg(sender, "&c&l(!)&c Not enough arguments");
+            return;
+        }
+
+        if (NPCAPI.npcExists(args[0])) {
+            NPCAPI.remove(args[0], true);
+            MinecraftUtils.msg(sender, "&a&l(!)&a Successfully removed " + args[0] + " npc!");
+        } else {
+            MinecraftUtils.msg(sender, "&c&l(!)&c NPC " + args[0] + " does not exists!");
+        }
+    }
+
+    @Subcommand("update")
+    @CommandAlias("vnpcupdate")
+    @CommandCompletion("<npcname>")
+    @Description("{@@commands.descriptions.npc.update}")
+    @Syntax("update <npcname> @range:1-999")
+    public void update(CommandSender sender, String @NotNull [] args) {
+        if (args.length == 0) {
+            MinecraftUtils.msg(sender, "&c&l(!)&c Not enough arguments");
+            return;
+        }
+
+        if (NPCAPI.npcExists(args[0])) {
+            NPCAPI.updateNPC(args[0], NPCAPI.getNPC(args[0]).getLocation(), args.length > 1 ? args[1].matches("-?(0|[1-9]\\d*)") ? Integer.parseInt(args[1]) : NPCAPI.getNPCMap().get(args[0], ObjectMap.Pair.create(NPCAPI.getNPC(args[0]), 1)).getValue() : NPCAPI.getNPCMap().get(args[0], ObjectMap.Pair.create(NPCAPI.getNPC(args[0]), 1)).getValue(), args.length > 1);
+            MinecraftUtils.msg(sender, "&a&l(!)&a Successfully updated " + args[0] + " npc!");
+        } else {
+            MinecraftUtils.msg(sender, "&c&l(!)&c NPC " + args[0] + " does not exists!");
+        }
+    }
+
+    @Subcommand("updateall")
+    @CommandAlias("vnpcupdateall")
+    @CommandCompletion("<npcname>")
+    @Description("{@@commands.descriptions.npc.updateall}")
+    @Syntax("update <npcname> @range:1-999")
+    public void updateAll(CommandSender sender, String @NotNull [] args) {
+        NPCAPI.updateAll();
+        MinecraftUtils.msg(sender, "&a&l(!)&a All NPCs have been updated!");
     }
 }
