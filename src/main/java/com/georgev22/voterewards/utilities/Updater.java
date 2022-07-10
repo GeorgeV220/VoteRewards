9e5ae7f233c680656727650ea1586692efce7ac1
George V <georgev22.github@gmail.com>
2022-07-10 23:58:02 +0300

    feat(Updater): Updater is does no longer need the version.md file

--- src/main/java/com/georgev22/voterewards/utilities/Updater.java
+++ src/main/java/com/georgev22/voterewards/utilities/Updater.java
@@ -2,35 +2,31 @@ package com.georgev22.voterewards.utilities;
 
 import com.georgev22.api.minecraft.MinecraftUtils;
 import com.georgev22.voterewards.VoteRewardPlugin;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class Updater {
 
     private final VoteRewardPlugin voteRewardPlugin = VoteRewardPlugin.getInstance();
     private final String localVersion = voteRewardPlugin.getDescription().getVersion();
-    private final String BASE_URL = "https://raw.githubusercontent.com/Project-Alterra/VoteRewards/master/version.md";
     private String onlineVersion;
 
     public Updater() {
         Bukkit.getScheduler().runTaskTimerAsynchronously(voteRewardPlugin, () -> {
             voteRewardPlugin.getLogger().info("Checking for Updates ... ");
             try {
-                System.setProperty("http.agent", "Chrome");
-                HttpsURLConnection con = (HttpsURLConnection) new URL(BASE_URL).openConnection();
-
-                con.setDoOutput(true);
-
-                con.setRequestMethod("GET");
-
-                onlineVersion = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
-
+                onlineVersion = getOnlineVersion();
             } catch (Exception ex) {
 
                 MinecraftUtils.debug(voteRewardPlugin, "Failed to check for an update on Git.", "Either Git or you are offline or are slow to respond.");
@@ -61,15 +57,7 @@ public class Updater {
         Bukkit.getScheduler().runTaskAsynchronously(voteRewardPlugin, () -> {
             MinecraftUtils.msg(player, "&e&lUpdater &8» &6Checking for Updates ...");
             try {
-                System.setProperty("http.agent", "Chrome");
-                HttpsURLConnection con = (HttpsURLConnection) new URL(BASE_URL).openConnection();
-
-                con.setDoOutput(true);
-
-                con.setRequestMethod("GET");
-
-                onlineVersion = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
-
+                onlineVersion = getOnlineVersion();
             } catch (Exception ex) {
                 MinecraftUtils.msg(player, "&e&lUpdater &8» &cFailed to check for an update on Git.");
                 MinecraftUtils.msg(player, "&e&lUpdater &8» &cEither Git or you are offline or are slow to respond.");
@@ -98,7 +86,7 @@ public class Updater {
 
 
     private int compareVersions(@NotNull String version1, @NotNull String version2) {
-        if (version1.contains("SNAPSHOT") | version1.contains("SNAPSHOT")) {
+        if (version1.contains("alpha") | version1.contains("beta")) {
             return -1;
         }
 
@@ -118,6 +106,27 @@ public class Updater {
             }
         }
         return comparisonResult;
+    }
+
+    private @NotNull String getOnlineVersion() throws IOException {
+        System.setProperty("http.agent", "Chrome");
+        HttpsURLConnection con = (HttpsURLConnection) new URL("https://api.github.com/repos/GeorgeV220/VoteRewards/tags").openConnection();
+
+        con.setDoOutput(true);
+
+        con.setRequestMethod("GET");
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        String jsonText = sb.toString();
+        JsonElement jsonElement = JsonParser.parseString(jsonText);
+        JsonArray jsonArray = jsonElement.getAsJsonArray();
+
+        return jsonArray.get(0).getAsJsonObject().get("name").getAsString().replace("\"", "");
     }
 
 }
